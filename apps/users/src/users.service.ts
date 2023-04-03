e6282d2eee8c87ce435f5c3511814cade6e74ed1
Mehradi <mehradi.info@gmail.com>
2023-04-04 00:51:37 +0200

    Updating user.service

--- apps/users/src/users.service.ts
+++ apps/users/src/users.service.ts
@@ -1,17 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserInput } from './dto/create-user.input';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
+  private readonly users: User[] = [];
   create(createUserInput: CreateUserInput) {
-    return 'This action adds a new user';
+    this.users.push(createUserInput);
+    return createUserInput;
   }
 
   findAll() {
-    return `This action returns all users`;
+    return this.users;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  findOne(id: string) {
+    return this.users.find((user) => user.id === id);
   }
 }
