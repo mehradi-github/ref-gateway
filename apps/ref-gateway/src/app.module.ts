561990221111e6030e812413a83458f4b182020d
Mehradi <mehradi.info@gmail.com>
2023-04-04 02:56:15 +0200

    Adding Apollo Gateway

--- apps/ref-gateway/src/app.module.ts
+++ apps/ref-gateway/src/app.module.ts
@@ -1,9 +1,30 @@
+import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { IntrospectAndCompose } from '@apollo/gateway';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloGatewayDriverConfig>({
+      driver: ApolloGatewayDriver,
+      gateway: {
+        supergraphSdl: new IntrospectAndCompose({
+          subgraphs: [
+            {
+              name: 'users',
+              url: 'http://localhost:3000/graphql',
+            },
+            {
+              name: 'posts',
+              url: 'http://localhost:3001/graphql',
+            },
+          ],
+        }),
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
